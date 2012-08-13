10592331a46737a720b900ac8c17a062eb5d739a
Jon Zobrist <jon@jonzobrist.com>
2012-08-13 12:58:03 -0600

    Fixed double }} typo in bluesun-setup.sh file

--- bluesun-setup/bluesun-setup.sh
+++ bluesun-setup/bluesun-setup.sh
@@ -4,6 +4,38 @@
 # Provides:		bluesun-setup
 # Short-Description:	S3 file and DB checkout and deploy script
 ### END INIT INFO
+#
+# Author : Jon Zobrist <jon@jonzobrist.com>
+# Homepage : http://www.jonzobrist.com
+# License : BSD http://en.wikipedia.org/wiki/BSD_license
+# Copyright (c) 2012, Jon Zobrist
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Purpose : This script backs up directories & MySQL to a bucket in S3, and restores them
+# Usage : bluesun-setup.sh [start|stop|updateS3]
+# WARNING : Running this script with start will DELETE your local versions of whatever you have it set to backup
+# WARNING : Running this script with start will DELETE your local versions of whatever you have it set to backup
+# WARNING : Running this script with start will DELETE your local versions of whatever you have it set to backup
 
 if [ -f /etc/bluesun-setup/server.conf ]
  then
@@ -160,10 +192,10 @@ case "${1}" in
 		cd ${DIR}
 		if [ ${DEBUG} -eq 1 ]
 		 then
-			if [ ${DEBUG} -eq 1 ]; then echo "tar -zvcf ${TMPDIR}}/${FILENAME} *"; fi
+			if [ ${DEBUG} -eq 1 ]; then echo "tar -zvcf ${TMPDIR}/${FILENAME} *"; fi
 			tar -zvcf ${TMPDIR}/${FILENAME} *
 		 else
-			if [ ${DEBUG} -eq 1 ]; then echo "tar -zcf ${TMPDIR}}/${FILENAME} *"; fi
+			if [ ${DEBUG} -eq 1 ]; then echo "tar -zcf ${TMPDIR}/${FILENAME} *"; fi
 			tar -zcf ${TMPDIR}/${FILENAME} *
 		fi
 		if [ ${DEBUG} -eq 1 ]; then echo "Uploading to S3 ${S3_BUCKET} at `date`"; fi
