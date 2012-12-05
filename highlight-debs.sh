37ace60c1fee095afe00bdd6d2242a4df1d497d4
Jon Zobrist <jon@jonzobrist.com>
2012-12-05 16:40:40 -0700

    added highlight-debs.sh for quick scanning my debian repository to see package versions

---
+++ highlight-debs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Author : Jon Zobrist <jon@jonzobrist.com>
+# Homepage : http://www.jonzobrist.com
+# License : BSD http://en.wikipedia.org/wiki/BSD_license
+# Copyright (c) 2012, Jon Zobrist
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Purpose : given a path, default is current working directory, find all files named .deb and highlight their name only
+# Usage : highlight-dbs.sh [PATH]
+
+if [ "${1}" ]
+ then
+ FIND_PATH="${1}"
+else
+ FIND_PATH="$(pwd)"
+fi
+
+find ${FIND_PATH} -iname "*deb" -type f | grep --color -oP '[A-z0-9-_.~]*\.deb'
