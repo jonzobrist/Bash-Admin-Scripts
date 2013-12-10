3d4f05a8bbbe0e4b7712cf4fc424178e9edc65e0
Jon Zobrist <zob@amazon.com>
2013-12-09 16:25:00 -0800

    Including BSD license in header

--- sshsetup.sh
+++ sshsetup.sh
@@ -34,6 +34,35 @@
 # if you're a real host on the internet with working reverse dns then
 # you're all set
 # now you may need to edit the last
+#
+# Author : Jon Zobrist <jon@jonzobrist.com>
+# Homepage : http://www.jonzobrist.com
+# License : BSD http://en.wikipedia.org/wiki/BSD_license
+# Copyright (c) 2013, Jon Zobrist
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Purpose : This script uses the s3ls command to list all files in a bucket, root dir
+# Usage : gather-public-ssh-keys.sh [Directory]
 
 
 @myagent = `ssh-agent`;
