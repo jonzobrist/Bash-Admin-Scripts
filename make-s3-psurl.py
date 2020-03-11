e3f47d74a1c4ba979e078452937340c57ce4f217
Jon Zobrist <jon@jonzobrist.com>
2020-03-11 08:54:09 -0700

    fixed default value handling from function to argparse

--- make-s3-psurl.py
+++ make-s3-psurl.py
@@ -18,11 +18,11 @@ import argparse
 from botocore.client import Config
 
 
-def presign_url(bucket, key, expiry=604800):
+def presign_url(bucket, key, expiry):
     # Get the service client with sigv4 configured
     s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
     # Generate the URL to get 'key-name' from 'bucket-name'
-    # URL expires in 604800 seconds (seven days)
+    # URL expires in expiry seconds (default 86400 seconds or 1 day)
     url = s3.generate_presigned_url(
         ClientMethod='get_object',
         Params={
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make a S3 presigned URL for key in bucket.')
     parser.add_argument('-b', '-bucket', dest='s3_bucket', type=str, help='Bucket the key is in', required=True)
     parser.add_argument('-k', '-key', dest='s3_key', type=str, help='Key to create PSU for', required=True)
-    parser.add_argument('-t', '-expiry', dest='s3_expiry', type=str, help='Key to create PSU for', required=False)
+    parser.add_argument('-t', '-expiry', dest='s3_expiry', type=str, help='Key to create PSU for', required=False, default=86400)
     args = parser.parse_args()
     s3_url = presign_url(args.s3_bucket, args.s3_key, args.s3_expiry)
     print(s3_url)
