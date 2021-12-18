a0fe07620a656e09efb1335b8f7f99a6951c89c8
Jon Zobrist <jon@jonzobrist.com>
2021-12-18 10:30:00 -0800

    env improvements

--- pihole/env.sh
+++ pihole/env.sh
@@ -15,3 +15,9 @@ declare -x N_DNS="1.1.1.1"
 declare -x TZ="US/Pacific"
 declare -x IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')"
 declare -x IP="${IP_LOOKUP}"
+
+# declare -x IP="192.168.1.254" #Or set it statically
+# declare -x IPV4_ADDRESS="192.168.1.254" #Override IPv4 manually
+# declare -x IPV6_ADDRESS="::" #Override IPv6 manually
+
+declare -x NET_TYPE="bridge" # can change to host if you want to use pihole DHCP
