ef7a831b00134b84d71a03325ebbc96d185324d0
Jon Zobrist <jon@jonzobrist.com>
2021-12-18 10:36:08 -0800

    untested changes to setup, pushing so i can test

--- pihole/setup-pihole.sh
+++ pihole/setup-pihole.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 # Pulls latest pihole scripts and starts
+# Requires you have docker running already, and you're using a linux host
+
+# Get the env.sh, and source it
+TF=$(mktemp)
+curl -o ${TF} https://raw.githubusercontent.com/jonzobrist/Bash-Admin-Scripts/master/pihole/env.sh
+perl -pi -e "s/pihole.local/$(hostname)/g" ${TF}
+. ${TF}
+mkdir -p ${PI_BASE}
+pushd ${PI_BASE} 
+mv ${TF} ./env.sh
 
 curl -O https://raw.githubusercontent.com/jonzobrist/Bash-Admin-Scripts/master/pihole/update-pihole.sh
-curl -O https://raw.githubusercontent.com/jonzobrist/Bash-Admin-Scripts/master/pihole/env.sh
 chmod uog+x update-pihole.sh
-perl -pi -e "s/pihole.local/$(hostname)/g" env.sh
+
+# This takes the output of your local hostname command and replaces all pihole.local in the env.sh
 ./update-pihole.sh
+
+popd
+
