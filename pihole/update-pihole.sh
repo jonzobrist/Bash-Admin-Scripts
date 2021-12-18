4dcd2480c7d3ef3c69cae0f35bd4242759a8abe0
Jon Zobrist <jon@jonzobrist.com>
2021-12-18 09:18:35 -0800

    cleanup and force update

--- pihole/update-pihole.sh
+++ pihole/update-pihole.sh
@@ -19,12 +19,15 @@ if [ -f "env.sh" ]
     declare -x PI_LIGHTTPD="${PI_BASE}/etc-lighttpd:/etc/lighttpd"
     declare -x IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')"
     declare -x IP="${IP:-$IP_LOOKUP}"
-    declare -x VIRTUAL_HOST="${VIRTUAL_HOST:-taco.zlyxy.me}"
+    declare -x VIRTUAL_HOST="${VIRTUAL_HOST:-pihole.local}"
     declare -x SERVER_PORT="${SERVER_PORT:-80}"
     declare -x M_DNS="${M_DNS:-127.0.0.1}"
     declare -x N_DNS="${N_DNS:-1.1.1.1}"
     declare -x TZ="${TZ:-US/Pacific}"
 fi
+
+declare -x FORCE_UPDATE=${FORCE_UPDATE:-0}
+declare -x UPDATE=0
 
 if [ -z ${IP} ] || [ -z ${IP_LOOKUP} ] || [ -z ${N_DNS} ] || [ -z ${PI_BASE} ] || [ -z ${PI_DNSM} ] || [ -z ${PI_ETC} ] || [ -z ${PI_LIGHTTPD} ] || [ -z ${PI_LOG} ] || [ -z ${SERVER_PORT} ] || [ -z ${TZ} ] || [ -z ${VIRTUAL_HOST} ] || [ -z ${WEB_PASS} ]
  then
@@ -49,10 +52,21 @@ else
     DEBUG "${UPDATE_OUTPUT}"
     NO_UPDATE=$(echo ${UPDATE_OUTPUT} | grep 'Image is up to date')
     DEBUG "Var NO_UPDATE = ${NO_UPDATE}"
-    if [ ${RETCODE} == 0 ] && [ -z "${NO_UPDATE}" ]
+    if [ ${RETCODE} -eq 0 ] && [ -z "${NO_UPDATE}" ]
      then
+        declare -x UPDATE=1
+    fi
+    if [ ${FORCE_UPDATE} -eq 1 ]
+     then
+        declare -x UPDATE=1
+    fi
+    if [ ${UPDATE} -eq 1 ]
+     then
+        DEBUG "Wipping pihole: docker rm -f pihole"
         docker rm -f pihole
-        docker run -d --name pihole --hostname taco --net=host -e TZ="${TZ}" -e WEBPASSWORD="${WEB_PASS}" -e IPV4_ADDRESS="${IP}" -e VIRTUAL_HOST="${VIRTUAL_HOST}" -v ${PI_ETC} -v ${PI_DNSM} --dns=${M_DNS} --dns=${N_DNS} --restart=unless-stopped --cap-add=NET_ADMIN pihole/pihole:latest
+        DEBUG "Running Pihole in Docker:"
+	DEBUG "docker run -d --name pihole --hostname pihole --net=host -e TZ=\"${TZ}\" -e WEBPASSWORD=\"${WEB_PASS}\" -e IPV4_ADDRESS=\"${IP}\" -e VIRTUAL_HOST=\"${VIRTUAL_HOST}\" -v ${PI_ETC} -v ${PI_DNSM} --dns=${M_DNS} --dns=${N_DNS} --restart=unless-stopped --cap-add=NET_ADMIN pihole/pihole:latest"
+        docker run -d --name pihole --hostname pihole --net=host -e TZ="${TZ}" -e WEBPASSWORD="${WEB_PASS}" -e IPV4_ADDRESS="${IP}" -e VIRTUAL_HOST="${VIRTUAL_HOST}" -v ${PI_ETC} -v ${PI_DNSM} --dns=${M_DNS} --dns=${N_DNS} --restart=unless-stopped --cap-add=NET_ADMIN pihole/pihole:latest
     else
         echo "Docker images not updated, skipping restart at $(date)"
     fi
